7ce1fb9d31dd3b501fcfbf7bd54bc94eb22ab36b
Maria-Varghese <maria.varghese@mths.ca>
2024-09-20 10:45:27 -0400

    changed my name

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Maria
- * Created on: Sep 2024
- * This program says hello World
+ * Created on: Sep 2023
+ * This program says 'Hello Maria'
 */
 
 basic.showString('Hello, Maria!')
